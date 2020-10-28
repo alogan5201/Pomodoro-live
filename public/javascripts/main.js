ef2ae92779895f28a955275df5cd4e063899d24d
alogan5201 <andrewthomaslogan@gmail.com>
2020-10-27 22:27:08 -0400

    update

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,136 @@
-var unique = require('uniq');
+/*
+	Solid State by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+(function ($) {
+  var $window = $(window),
+    $body = $("body"),
+    $header = $("#header"),
+    $banner = $("#banner");
+  $scroll = $(window).scrollTop();
 
-console.log(unique(data));
+  // Breakpoints.
+  breakpoints({
+    xlarge: "(max-width: 1680px)",
+    large: "(max-width: 1280px)",
+    medium: "(max-width: 980px)",
+    small: "(max-width: 736px)",
+    xsmall: "(max-width: 480px)",
+  });
+
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
+
+  // Header.
+  /* if ($banner.length > 0 && $header.hasClass("entry")) {
+      $window.on("resize", function () {
+        $window.trigger("scroll");
+      });
+  
+      $banner.scrollex({
+        bottom: $header.outerHeight(),
+        terminate: function () {
+          $header.addClass("alt");
+        },
+        enter: function () {
+          $header.removeClass("alt");
+        },
+        leave: function () {
+          $header.addClass("alt");
+        },
+      });
+    }
+  */
+
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 40) {
+      $header.addClass("alt");
+    } else {
+      $header.removeClass("alt");
+    }
+  });
+
+  // Menu.
+  var $menu = $("#menu");
+
+  $menu._locked = false;
+
+  $menu._lock = function () {
+    if ($menu._locked) return false;
+
+    $menu._locked = true;
+
+    window.setTimeout(function () {
+      $menu._locked = false;
+    }, 350);
+
+    return true;
+  };
+
+  $menu._show = function () {
+    if ($menu._lock()) $body.addClass("is-menu-visible");
+  };
+
+  $menu._hide = function () {
+    if ($menu._lock()) $body.removeClass("is-menu-visible");
+  };
+
+  $menu._toggle = function () {
+    if ($menu._lock()) $body.toggleClass("is-menu-visible");
+  };
+
+  $menu
+    .appendTo($body)
+    .on("click", function (event) {
+      event.stopPropagation();
+
+      // Hide.
+      $menu._hide();
+    })
+    .find(".inner")
+    .on("click", ".close", function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      event.stopImmediatePropagation();
+
+      // Hide.
+      $menu._hide();
+    })
+    .on("click", function (event) {
+      event.stopPropagation();
+    })
+    .on("click", "a", function (event) {
+      var href = $(this).attr("href");
+
+      event.preventDefault();
+      event.stopPropagation();
+
+      // Hide.
+      $menu._hide();
+
+      // Redirect.
+      window.setTimeout(function () {
+        window.location.href = href;
+      }, 350);
+    });
+
+  $body
+    .on("click", 'a[href="#menu"]', function (event) {
+      event.stopPropagation();
+      event.preventDefault();
+
+      // Toggle.
+      $menu._toggle();
+    })
+    .on("keydown", function (event) {
+      // Hide on escape.
+      if (event.keyCode == 27) $menu._hide();
+    });
+})(jQuery);
